9d1a23849060eb884869a904d9f187627741394d
umair86450 <2001muhammadumair@gmail.com>
2025-08-30 23:56:53 +0500

    Inital commit for simple and packaged apps

---
+++ packaged-app/src/packaged_app/main.py
@@ -0,0 +1,3 @@
+def main():
+    print("Name: Hafiz Muhammad Umair")
+    print("PIAIC Registration Number: PIAIC204291")
---
+++ simple-app/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Name: Hafiz Muhammad Umair")
+    print("PIAIC Registration Number: PIAIC204291")
+
+
+if __name__ == "__main__":
+    main()
